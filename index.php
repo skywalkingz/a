469edb6313f2f877007ef38056eb5d7086fc385c
skywalkingz <62224513+skywalkingz@users.noreply.github.com>
2020-05-25 17:00:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> diffusive.xyz </title>
+	<!-- favicon -->
+	<link rel="shortcut icon" href="assets/images/favicon.html" type="image/x-icon">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Plugin css -->
+	<link rel="stylesheet" href="assets/css/plugin.css">
+
+	<!-- stylesheet -->
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+	<!-- preloader area start -->
+	<div class="preloader" id="preloader">
+		<div class="loader loader-1">
+			<div class="loader-outter"></div>
+			<div class="loader-inner"></div>
+		</div>
+	</div>
+	<!-- preloader area end -->
+
+
+	<!-- Hero Area Start -->
+	<div class="hero-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5 d-flex align-self-center">
+					<div class="left-content">
+						<div class="content">
+							<h5 class="subtitle">
+								DISCORD STRESS SERVICES
+							</h5>
+							<h1 class="title">
+									DIFFUSIVE.XYZ
+							</h1>
+							<p class="text">
+									JOIN OUR DISCORD SERVER
+							</p>
+							<div class="links">
+								<a href="discord.php" class="mybtn1 link1">join!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-7">
+					<div class="hero-img2 d-block d-md-none">
+						<img src="assets/images/heroarea.png" alt="">
+					</div>
+					<div class="hero-img d-none d-md-block">
+						<img class="img-fluid full-image" src="assets/images/heroarea.png" alt="">
+						<img class="shape phone" src="assets/images/h-shapes/phone.png" alt="">
+						<img class="shape man" src="assets/images/h-shapes/man222.png" alt="">
+						<img class="shape ripple" src="assets/images/h-shapes/ripple.png" alt="">
+						<img class="shape ripple2" src="assets/images/h-shapes/ripple1.png" alt="">
+						<img class="shape bitcoin1" src="assets/images/h-shapes/bitcoin1.png" alt="">
+						<img class="shape bitcoin2" src="assets/images/h-shapes/bitcoin2.png" alt="">
+						<img class="shape shape-icon" src="assets/images/h-shapes/shape.png" alt="">
+						<img class="shape award-bg" src="assets/images/h-shapes/award/bg.png" alt="">
+						<img class="shape award" src="assets/images/h-shapes/award/award.png" alt="">
+						<img class="shape gift-bg" src="assets/images/h-shapes/giftbox/bg.png" alt="">
+						<img class="shape gift" src="assets/images/h-shapes/giftbox/gift.png" alt="">
+						<img class="shape shield-bg" src="assets/images/h-shapes/shield/bg.png" alt="">
+						<img class="shape shield" src="assets/images/h-shapes/shield/shield.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Hero Area End -->
+
+	<!-- Featured Game Area Start -->
+	<section class="featured-game">
+		<!-- Features Area Start -->
+		<div class="features">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="feature-box">
+							<div class="feature-box-inner">
+								<div class="row">
+									<div class="col-lg-4 col-md-6">
+										<div class="single-feature">
+											<div class="icon one">
+												<img src="assets/images/feature/payout.png" alt="">
+											</div>
+											<div class="content">
+												<h4 class="title">
+													Payment Methods
+												</h4>
+												<a href="discord.php" class="link">Paypal, Skrill and Bitcoin <i class="fas fa-arrow-right"></i></a>
+											</div>
+										</div>
+									</div>
+									
+									<div class="col-lg-4 col-md-6">
+										<div class="single-feature">
+											<div class="icon three">
+												<img src="assets/images/feature/icon3.png" alt="">
+											</div>
+											<div class="content">
+												<h4 class="title">
+														Excelent Support
+												</h4>
+												<a href="discord.php" class="link">24/7 support <i class="fas fa-arrow-right"></i></a>
+												</div>
+											</div>
+									    </div>		
+		                            <div class="col-lg-4 col-md-6">
+										<div class="single-feature">
+											<div class="icon three">
+												<img src="assets/images/feature/icon3.png" alt="">
+											</div>
+											<div class="content">
+												<h4 class="title">
+														Cheap Services
+												</h4>
+												<a href="discord.php" class="link">Affordable Prices! <i class="fas fa-arrow-right"></i></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+
+	<!-- Prices -->
+	<section class="recent-winners">
+			<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-8 col-md-10">
+							<div class="section-heading">
+								<h2 class="title">
+										Plans
+								</h2>
+								<p class="text">
+									Here you can check our prices !
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="single-winer">
+								<div class="top-area">
+									<div class="left">
+										<h4 class="name">
+												1 Week Plan
+										</h4>
+									</div>
+								</div>
+								<div class="bottom-area">
+									<div class="center">
+											2$
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="single-winer">
+								<div class="top-area">
+									<div class="left">
+										<h4 class="name">
+												1 Month Plan
+										</h4>
+									</div>
+								</div>
+								<div class="bottom-area">
+									<div class="center">
+											5$
+									</div>
+									<div class="right">
+									</div>
+								</div>
+							</div>
+						</div>
+					<div class="col-lg-4">
+							<div class="single-winer">
+								<div class="top-area">
+									<div class="left">
+										<h4 class="name">
+												3 Months Plan
+										</h4>
+									</div>
+								</div>
+								<div class="bottom-area">
+									<div class="center">
+											10$
+									</div>
+									<div class="right">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-12 text-xl-center">
+							<a class="mybtn2" href="discord.php">Discord Server</a>
+						</div>
+					</div>
+			</div>
+		</section> 
+	<!-- Prices End -->
+
+	<!-- Footer Area Start -->
+	<footer class="footer" id="footer">
+		<div class="subscribe-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="subscribe-box">
+								<form action="#">
+							<div class="row">
+								<div class="col-lg-12">
+									<div class="heading-area">
+									<div class="copy-bg">
+			                          <div class="container">
+				                        <div class="row">
+					                      <div class="col-lg-5">
+						                    <div class="center-area">
+							                  <p>Copyright © 2020. All Rights Reserved By skywalker</a>
+							                  </p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>							
+</footer>
+	<!-- Footer Area End -->
+
+	<!-- Back to Top Start -->
+	<div class="bottomtotop">
+		<i class="fas fa-chevron-right"></i>
+	</div>
+	<!-- Back to Top End -->
+
+	<!-- jquery -->
+	<script src="assets/js/jquery.js"></script>
+	<!-- popper -->
+	<script src="assets/js/popper.min.js"></script>
+	<!-- bootstrap -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- plugin js-->
+	<script src="assets/js/plugin.js"></script>
+
+	<!-- MpusemoverParallax JS-->
+	<script src="assets/js/TweenMax.js"></script>
+	<script src="assets/js/mousemoveparallax.js"></script>
+	<!-- main -->
+	<script src="assets/js/main.js"></script>
+	</section>
+</body>
+</html>
